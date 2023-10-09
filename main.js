99a4028139e2a15a291cfb3b59f2ac1107ac8378
Shotiko11 <shotikokokilashvili9@gmail.com>
2023-10-09 15:57:18 +0400

    next commit

--- main.js
+++ main.js
@@ -13,13 +13,18 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setClearColor(0x000000, 0);
 document.body.appendChild(renderer.domElement);
 
-const torusRadius = 1; 
-const tubeRadius = 0.3; 
+const torusRadius = 1;
+const tubeRadius = 0.35;
 const torusSegments = 16;
 const tubeSegments = 100;
 
-const geometry = new THREE.TorusGeometry(torusRadius, tubeRadius, torusSegments, tubeSegments);
-const orangeMaterial = new THREE.MeshStandardMaterial({ color: 0xFFA500 }); 
+const geometry = new THREE.TorusGeometry(
+  torusRadius,
+  tubeRadius,
+  torusSegments,
+  tubeSegments
+);
+const orangeMaterial = new THREE.MeshStandardMaterial({ color: 0xffa500 });
 const torus = new THREE.Mesh(geometry, orangeMaterial);
 scene.add(torus);
 
@@ -39,26 +44,26 @@ scene.add(directionalLight);
 let isDragging = false;
 let previousMousePosition = {
   x: 0,
-  y: 0
+  y: 0,
 };
 
-document.addEventListener('mousedown', (event) => {
+document.addEventListener("mousedown", (event) => {
   isDragging = true;
   previousMousePosition = {
     x: event.clientX,
-    y: event.clientY
+    y: event.clientY,
   };
 });
 
-document.addEventListener('mouseup', () => {
+document.addEventListener("mouseup", () => {
   isDragging = false;
 });
 
-document.addEventListener('mousemove', (event) => {
+document.addEventListener("mousemove", (event) => {
   if (isDragging) {
     const deltaMove = {
       x: event.clientX - previousMousePosition.x,
-      y: event.clientY - previousMousePosition.y
+      y: event.clientY - previousMousePosition.y,
     };
 
     torus.rotation.x += deltaMove.y * 0.005;
@@ -66,7 +71,7 @@ document.addEventListener('mousemove', (event) => {
 
     previousMousePosition = {
       x: event.clientX,
-      y: event.clientY
+      y: event.clientY,
     };
   }
 });
@@ -82,13 +87,7 @@ function animate() {
 
 animate();
 
-
-
-
 // dsadsadsadasdasdasdasdas
-
-
-
 
 document.addEventListener("DOMContentLoaded", function () {
   let video1 = document.getElementById("background-video");
@@ -102,89 +101,100 @@ document.addEventListener("DOMContentLoaded", function () {
   let isScrolling = false;
 
   document.addEventListener("wheel", function (event) {
-      if (event.deltaY > 0) {
-          switch (activeVideo) {
-              case video1:
-                  video1.style.display = "none";
-                  video2.style.display = "block";
-                  activeVideo = video2;
-                  break;
-              case video2:
-                  video2.style.display = "none";
-                  video3.style.display = "block";
-                  activeVideo = video3;
-                  break;
-              case video3:
-                  video3.style.display = "none";
-                  video4.style.display = "block";
-                  activeVideo = video4;
-                  break;
-              case video4:
-                  video4.style.display = "none";
-                  video5.style.display = "block";
-                  activeVideo = video5;
-                  break;
-              case video5:
-                  video5.style.display = "none";
-                  video6.style.display = "block";
-                  activeVideo = video6;
-                  break;
-              case video6:
-                  video6.style.display = "none";
-                  video1.style.display = "block";
-                  activeVideo = video1;
-                  break;
-              default:
-                  break;
-          }
-      } else {
-          // Scroll up
-          switch (activeVideo) {
-              case video1:
-                  video1.style.display = "none";
-                  video6.style.display = "block";
-                  activeVideo = video6;
-                  break;
-              case video2:
-                  video2.style.display = "none";
-                  video1.style.display = "block";
-                  activeVideo = video1;
-                  break;
-              case video3:
-                  video3.style.display = "none";
-                  video2.style.display = "block";
-                  activeVideo = video2;
-                  break;
-              case video4:
-                  video4.style.display = "none";
-                  video3.style.display = "block";
-                  activeVideo = video3;
-                  break;
-              case video5:
-                  video5.style.display = "none";
-                  video4.style.display = "block";
-                  activeVideo = video4;
-                  break;
-              case video6:
-                  video6.style.display = "none";
-                  video5.style.display = "block";
-                  activeVideo = video5;
-                  break;
-              default:
-                  break;
-          }
+    if (event.deltaY > 0) {
+      switch (activeVideo) {
+        case video1:
+          video1.style.display = "none";
+          video2.style.display = "block";
+          activeVideo = video2;
+          break;
+        case video2:
+          video2.style.display = "none";
+          video3.style.display = "block";
+          activeVideo = video3;
+          break;
+        case video3:
+          video3.style.display = "none";
+          video4.style.display = "block";
+          activeVideo = video4;
+          break;
+        case video4:
+          video4.style.display = "none";
+          video5.style.display = "block";
+          activeVideo = video5;
+          break;
+        case video5:
+          video5.style.display = "none";
+          video6.style.display = "block";
+          activeVideo = video6;
+          break;
+        case video6:
+          video6.style.display = "none";
+          video1.style.display = "block";
+          activeVideo = video1;
+          break;
+        default:
+          break;
       }
+    } else {
+      // Scroll up
+      switch (activeVideo) {
+        case video1:
+          video1.style.display = "none";
+          video6.style.display = "block";
+          activeVideo = video6;
+          break;
+        case video2:
+          video2.style.display = "none";
+          video1.style.display = "block";
+          activeVideo = video1;
+          break;
+        case video3:
+          video3.style.display = "none";
+          video2.style.display = "block";
+          activeVideo = video2;
+          break;
+        case video4:
+          video4.style.display = "none";
+          video3.style.display = "block";
+          activeVideo = video3;
+          break;
+        case video5:
+          video5.style.display = "none";
+          video4.style.display = "block";
+          activeVideo = video4;
+          break;
+        case video6:
+          video6.style.display = "none";
+          video5.style.display = "block";
+          activeVideo = video5;
+          break;
+        default:
+          break;
+      }
+    }
 
-      isScrolling = true;
+    isScrolling = true;
 
-      setTimeout(function () {
-          isScrolling = false;
-      }, 100);
+    setTimeout(function () {
+      isScrolling = false;
+    }, 100);
   });
 
   window.addEventListener("scroll", function (event) {
-      if (isScrolling) {
-          event.preventDefault();
-      }
+    if (isScrolling) {
+      event.preventDefault();
+    }
   });
+
+  setTimeout(function () {
+    const loadingScreen = document.querySelector(".loading-screen");
+    loadingScreen.style.display = "none";
+
+    const h1Element = document.querySelector("h1");
+    h1Element.style.display = "block";
+
+    const animationContainer = document.querySelector(".animation-container");
+    animationContainer.classList.add("animation-started");
+  }, 5000);
 });
